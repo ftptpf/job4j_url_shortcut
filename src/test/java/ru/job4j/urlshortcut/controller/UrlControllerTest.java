8e4e6131050fb0d7519d6f9059ac3b1c3ef933cf
ftptpf <inboxru@yandex.ru>
2023-04-17 09:39:32 +0300

    UrlController test

---
+++ src/test/java/ru/job4j/urlshortcut/controller/UrlControllerTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.urlshortcut.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.job4j.urlshortcut.UrlShortcutApplication;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest(classes = UrlShortcutApplication.class)
+@AutoConfigureMockMvc
+class UrlControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @WithMockUser
+    void whenConvertUrlToCodeAndSaveToDatabase() {
+        mockMvc.perform(post("/api/v1/url/convert"))
+                .andDo()
+                .andExpect(status().is2xxSuccessful());
+
+    }
+}
