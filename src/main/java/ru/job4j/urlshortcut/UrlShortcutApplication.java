bba551b3699db9286588b86b2759322178ea82a7
ftptpf <Nb49Rtdd>
2023-04-21 21:52:55 +0300

    Liquibase bean

--- src/main/java/ru/job4j/urlshortcut/UrlShortcutApplication.java
+++ src/main/java/ru/job4j/urlshortcut/UrlShortcutApplication.java
@@ -1,10 +1,29 @@
 package ru.job4j.urlshortcut;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class UrlShortcutApplication {
+public class UrlShortcutApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(UrlShortcutApplication.class);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:db/dbchangelog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UrlShortcutApplication.class, args);
