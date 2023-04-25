5e970380cbcadd6dd17eab556200ae09218788c0
ftptpf <inboxru@yandex.ru>
2023-04-25 09:35:13 +0300

    Add property source to tests

--- src/test/java/ru/job4j/urlshortcut/controller/WebSiteControllerTest.java
+++ src/test/java/ru/job4j/urlshortcut/controller/WebSiteControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@PropertySource("classpath:/application-test.properties")
 @Sql(scripts = "/db/scripts/002_dml_delete_from_table_websites.sql")
 class WebSiteControllerTest {
 
