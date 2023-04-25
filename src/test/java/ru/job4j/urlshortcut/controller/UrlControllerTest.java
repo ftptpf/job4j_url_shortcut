eb7da08491c23d61b42f3856daac0ab1b3585d07
ftptpf <inboxru@yandex.ru>
2023-04-25 16:59:50 +0300

    Test Property Source "classpath:db/liquibase-test.properties"

--- src/test/java/ru/job4j/urlshortcut/controller/UrlControllerTest.java
+++ src/test/java/ru/job4j/urlshortcut/controller/UrlControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.urlshortcut.dto.UrlDto;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:db/liquibase-test.properties")
 @Sql(scripts = "/db/scripts/004_dml_delete_from_table_urls.sql")
 class UrlControllerTest {
 
--- src/test/java/ru/job4j/urlshortcut/controller/WebSiteControllerTest.java
+++ src/test/java/ru/job4j/urlshortcut/controller/WebSiteControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.urlshortcut.dto.WebSiteDto;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:db/liquibase-test.properties")
 @Sql(scripts = "/db/scripts/002_dml_delete_from_table_websites.sql")
 class WebSiteControllerTest {
 
